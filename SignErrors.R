f267a85ea1d42963e196fafc2cee23b2fed4a932
Stevie Molinari <stephen.molinari@gmail.com>
2020-01-03 17:40:21 -0700

    initial sign error problem commit

---
+++ SignErrors.R
@@ -0,0 +1,6 @@
+#### Sign error problem ####
+### posted to twitter by Bill Chen @billchenpoker on 2017-11-30
+
+"A student can make a sign error independently in any step in a math problem. [If] each sign flip is less than 50% is less than 50% to occur, and an even number of sign flips gets you the right sign. Does the student necessarily have over a 505 change of getting the right sign"
+
+sum(eventerms_n)
