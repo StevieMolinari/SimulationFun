4e0a1ab9e728351ff925b522a6aa6aa12af37be1
Stevie Molinari <stephen.molinari@gmail.com>
2020-02-17 22:44:02 -0700

    an intial solution to a specific instance of the riddle

--- SignErrors.R
+++ SignErrors.R
@@ -1,6 +1,12 @@
 #### Sign error problem ####
+library(tidyverse)
 ### posted to twitter by Bill Chen @billchenpoker on 2017-11-30
 
-"A student can make a sign error independently in any step in a math problem. [If] each sign flip is less than 50% is less than 50% to occur, and an even number of sign flips gets you the right sign. Does the student necessarily have over a 505 change of getting the right sign"
+"A student can make a sign error independently in any step in a math problem. [If] each sign flip is less than 50% is less than 50% to occur, and an even number of sign flips gets you the right sign. Does the student necessarily have over a 50% chance of getting the right sign"
 
-sum(eventerms_n)
+probError <- 1/3
+nSteps <- 11
+probNErrors <- dbinom(0:nSteps, nSteps, probError)
+evenNumMistakesProbs<- probNErrors[seq(0, (floor(nSteps/2)*2), by = 2) + 1]
+
+sum(evenNumMistakesProbs)
