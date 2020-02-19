af26c11c2aee32f9d3240093dc5781f3eeb69288
ebb-earl-co <ebb-earl-co@pm.me>
2020-02-18 20:01:39 -0700

    Change README.md and edit SignErrors.R

--- SignErrors.R
+++ SignErrors.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 ### posted to twitter by Bill Chen @billchenpoker on 2017-11-30
 
-"A student can make a sign error independently in any step in a math problem. [If] each sign flip is less than 50% is less than 50% to occur, and an even number of sign flips gets you the right sign. Does the student necessarily have over a 50% chance of getting the right sign"
+"A student can make a sign error independently in any step in a math problem. [If] each sign flip is less than 50% to occur, and an even number of sign flips gets you the right sign, does the student necessarily have over a 50% chance of getting the right sign?"
 
 probError <- 1/3
 nSteps <- 11
